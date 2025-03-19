d9c1512bac678286431b6a0d63550fca8cdca0d6
NotBioWaste905 <andruhabobr@gmail.com>
2025-03-19 13:36:02 +0300

    Added YAML support, added an example

--- dialogue2graph/cli/main.py
+++ dialogue2graph/cli/main.py
@@ -1,4 +1,5 @@
 import click
+import yaml
 from dotenv import load_dotenv
 from .commands.generate_data import generate_data
 
@@ -13,12 +14,20 @@ def cli():
 
 @cli.command()
 @click.option("--env", "-e", help="Path to .env file", default=".env")
+@click.option("--cfg", "-c", help="Path to cfg.yml file", default="cfg.yml")
 @click.option("--topic", "-t", help="Topic for dialogue generation", required=True)
 @click.option("--output", "-o", help="Output file path", default="generated_data.json")
-def gen_data(env: str, topic: str, output: str):
+def gen_data(env: str, cfg: str, topic: str, output: str):
     """Generate dialogue data for a given topic"""
     load_dotenv(env)
-    generate_data(topic, output)
+    with open(cfg) as stream:
+        config: dict = {}
+        try:
+            config = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+    generate_data(topic, config, output)
 
 
 # @cli.command()
