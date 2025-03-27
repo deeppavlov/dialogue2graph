25876b67240f6a1ec015a505506ebdc3fbacd42f
Yuriy Peshkichev <peshkichev@gpu6.ipavlov.mipt.ru>
2025-03-27 07:26:38 +0300

    dev merged partially

---
+++ docs/source/about_us.rst
@@ -0,0 +1,8 @@
+About us
+=========
+
+Chatsky LLM-Autoconfig is developed by `DeepPavlov <https://deeppavlov.ai>`_.
+
+Founder and leader of the project -- 
+
+Developers -- 
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,22 +6,47 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Chatsky LLM-Autoconfiguration'
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../../dialogue2graph'))
+
+project = 'Chatsky LLM-Autoconfig'
 copyright = '2024, Denis Kuznetsov, Anastasia Voznyuk, Andrey Chirkin'
 author = 'Denis Kuznetsov, Anastasia Voznyuk, Andrey Chirkin'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode', 
+    'sphinx.ext.napoleon',
+    "sphinx.ext.extlinks",
+    'sphinx_autodoc_typehints',
+    'sphinxcontrib.apidoc',
+]
 
 templates_path = ['_templates']
-exclude_patterns = []
 
+autodoc_default_options = {
+    "members": True,
+    "undoc-members": False,
+    "private-members": False,
+    "special-members": "__call__",
+    "member-order": "bysource",
+    "exclude-members": "_abc_impl, model_fields, model_computed_fields, model_config",
+}
+
+apidoc_module_dir = '../../dialogue2graph'
+apidoc_output_dir = 'reference'
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = 'pydata_sphinx_theme'
+
+extlinks = {
+    'github_source_link': ("https://github.com/deeppavlov/chatsky-llm-autoconfig/tree/dev/%s", None),
+}
