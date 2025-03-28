df37e19adb14402dd1b64f6e01c554be112ea3a6
NotBioWaste905 <andruhabobr@gmail.com>
2025-03-28 14:10:31 +0300

    Added model_storage field in Pipeline

--- dialogue2graph/pipelines/core/pipeline.py
+++ dialogue2graph/pipelines/core/pipeline.py
@@ -1,9 +1,11 @@
-from typing import Union
+from typing import Union, Optional
 from pydantic import BaseModel, Field
+from dialogue2graph.pipelines.model_storage import ModelStorage
 from dialogue2graph.pipelines.core.algorithms import DialogAugmentation, DialogueGenerator, GraphGenerator, GraphExtender, InputParser
 
 
 class Pipeline(BaseModel):
+    model_storage: Optional[ModelStorage] = Field(default_factory=ModelStorage)
     steps: list[Union[InputParser, DialogueGenerator, DialogAugmentation, GraphGenerator, GraphExtender]] = Field(default_factory=list)
 
     def _validate_pipeline(self):
