febc53d128ba99b8d51aa6de4c5bb24de1e33b84
NotBioWaste905 <andruhabobr@gmail.com>
2025-03-28 13:32:00 +0300

    Switched to "alabaster" theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -45,13 +45,8 @@ apidoc_output_dir = 'reference'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 
 extlinks = {
     'github_source_link': ("https://github.com/deeppavlov/chatsky-llm-autoconfig/tree/dev/%s", None),
 }
-
-html_static_path = ['_static']
-html_css_files = [
-    'custom.css',
-]
