9e92651ef828b6d5e7220b503c2254e765073c62
NotBioWaste905 <andruhabobr@gmail.com>
2025-04-04 23:38:49 +0300

    Add ruff configuration and update code style checks to include all files

--- scripts/codestyle.py
+++ scripts/codestyle.py
@@ -2,8 +2,8 @@ import subprocess
 
 def _check():
     """Run ruff to check code style."""
-    subprocess.run(["ruff", "check", "./dialogue2graph"], check=True)
+    subprocess.run(["ruff", "check", "--fix", "."], check=True)
 
 def _format():
     """Run ruff to format code."""
-    subprocess.run(["ruff", "format", "./dialogue2graph"], check=True)
+    subprocess.run(["ruff", "format", "."], check=True)
